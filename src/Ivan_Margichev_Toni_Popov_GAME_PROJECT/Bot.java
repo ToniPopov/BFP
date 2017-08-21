784b8d5beb16f1dfbc35495864c688b36ff2a440
ToniPopov <tonitheonea@gmail.com>
2017-08-21 21:59:02 +0300

    remove Comments

--- src/Ivan_Margichev_Toni_Popov_GAME_PROJECT/Bot.java
+++ src/Ivan_Margichev_Toni_Popov_GAME_PROJECT/Bot.java
@@ -1,12 +1,6 @@
 package Ivan_Margichev_Toni_Popov_GAME_PROJECT;
 
 public class Bot extends GamePlayers{
-String v;
-	String c;
-	//TEST !
-    //new TEst
-	//test test
-	//now comentar
 
 	Bot(){
 		super(validateData("Computer"));
